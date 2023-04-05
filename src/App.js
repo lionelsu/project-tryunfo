0aafa84b83b09378357f85117b01c28c0ea00e08
lionelsu <lionel_suicune@hotmail.com>
2023-04-05 16:45:19 -0400

    req: 04 implemented render states form Card and Form components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardImage: '',
+    cardRare: 'normal',
+    cardTrunfo: false,
+  };
+
+  onChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    // console.log(name, value, type, checked);
+    const newState = type === 'checkbox' ? checked : value;
+    this.setState({ [name]: newState });
+  };
+
   render() {
     return (
       <div>
-        <h1>Tryunfo</h1>
-        <Form />
-        <Card />
+        <Form
+          onInputChange={ this.onChange }
+          values={ this.state }
+        />
+        <Card { ...this.state } />
       </div>
     );
   }
